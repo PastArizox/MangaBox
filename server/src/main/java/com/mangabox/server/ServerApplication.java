870936117d64b734ec1f95205eb22e0e11d9cc5e
PastArizox <arizoxfr@gmail.com>
2025-01-31 22:36:13 +0100

    Initialized Spring Boot

---
+++ server/src/main/java/com/mangabox/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.mangabox.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
