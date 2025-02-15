c1333978776935f3188e90711a43426dc94464a8
PastArizox <arizoxfr@gmail.com>
2025-02-15 23:23:24 +0100

    Added new dotenv package + removed manual dotenv config

--- server/src/main/java/com/mangabox/server/ServerApplication.java
+++ server/src/main/java/com/mangabox/server/ServerApplication.java
@@ -3,24 +3,10 @@ package com.mangabox.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class ServerApplication {
-
-    static {
-        Dotenv dotenv = Dotenv
-                .configure()
-                .ignoreIfMissing() // Don't crash if .env file is missing
-                .load();
-
-        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL", "jdbc:postgresql://localhost:5432/default_db"));
-        System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER", "default_user"));
-        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD", "default_password"));
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
-
 }
